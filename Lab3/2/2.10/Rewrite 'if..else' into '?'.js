6a5980eb6a6bc154c7d229f87998b6d94acbac97
Kes <chelovekrukzak@gmail.com>
2025-02-04 02:16:43 +0500

    ww1

---
+++ Lab3/2/2.10/Rewrite 'if..else' into '?'.js
@@ -0,0 +1,15 @@
+let message = (login == 'Employee') ? 'Hello' :
+                (login == "Director") ? "Greetings" :
+                (login == "") ? "No login":
+                ""
+;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
